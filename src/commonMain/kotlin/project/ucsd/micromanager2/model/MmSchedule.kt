9c5e57fd935d3f835177ef9345833917f8cd586c
Kyros Chow <kyroschow@gmail.com>
2020-07-17 18:21:43 -0700

    Add progress to MmSchedule

--- src/commonMain/kotlin/project/ucsd/micromanager2/model/MmSchedule.kt
+++ src/commonMain/kotlin/project/ucsd/micromanager2/model/MmSchedule.kt
@@ -4,5 +4,6 @@ data class MmSchedule(
     val id: String,
     val ownerId: String,
     val name: String,
+    val progress: Float, // solve progress of this schedule, between 0.0 to 1.0 inclusive
     val events: List<MmEvent>
 )
